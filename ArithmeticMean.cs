e081e89a6042cc9bac78049e46ac57651c3263ed
whymay <78863042+whymay@users.noreply.github.com>
2021-03-01 14:54:47 +0300

    Add files via upload

---
+++ ArithmeticMean.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithmetic_Mean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number1, number2;
+
+            Console.WriteLine("Write the first number");
+
+            number1 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Write the second number");
+            
+            number2 = Convert.ToDouble(Console.ReadLine());
+            
+            double result = (number1 + number2) / 2;
+            
+            Console.WriteLine(result);
+            
+            Console.ReadLine();
+        }
+    }
+}
