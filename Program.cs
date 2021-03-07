796d11c1991befbc36a8a2a6df459570036c33ae
whymay <78863042+whymay@users.noreply.github.com>
2021-03-07 19:19:46 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            double b;
+
+            Console.WriteLine("Write the first number");
+            
+            a = int.Parse(Console.ReadLine());
+
+            b = a % 2;
+
+            if (b == 0)
+            {
+                Console.WriteLine("The number is even");
+            }
+            else
+            {
+                Console.WriteLine("The number isn't even");
+            }
+
+        }
+    }
+}
